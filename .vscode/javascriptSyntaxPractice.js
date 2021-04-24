0dd38641713557122ea17ff250fa39e88b1b1090
uderek1 <duzoh@dons.usfca.edu>
2021-04-23 22:18:59 -0700

    first commit

---
+++ .vscode/javascriptSyntaxPractice.js
@@ -0,0 +1,16 @@
+function valueChecker(argument1,argument2){
+    if(argument1===argument2){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function sumCalculator(array){
+    var total=0
+    for (let index = 0; index < array.length; index++) {
+        total=total+array[index]
+    }
+    return total
+}
